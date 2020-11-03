0c695dd2c6d398721c7c4e9113283321852b099b
Tawhid <gem@tawhid.fios-router.home>
2020-11-02 21:53:13 -0500

    made score field

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ const button = document.querySelector('button');
 const player1 = document.querySelector('#player1');
 const player2 = document.querySelector('#player2');
 const output = document.querySelector('#output');
+const player1score = document.querySelector('#player1score');
+const player2score = document.querySelector('#player2score');
+
+let score1 = 0;
+let score2 = 0;
+
 
 button.addEventListener('click', () => {
     console.log('clicked')
     let rolls = [roll(6), roll(6)]
-    console.log(rolls)
 
     if(rolls[0] === rolls[1]){
         output.innerHTML = `Its a Draw!`
     } else if(rolls[0] > rolls[1]){
-        output.innerHTML = `Player 1 Wins`
+        output.innerHTML = `Player 1 Wins`;
+        score1++
+        player1score.innerHTML = `Player 1 = ${score1}`
     } else {
         output.innerHTML = `Player 2 Wins`
+        score2++
+        player2score.innerHTML = `Plater 2 = ${score2}`
     }
 
     player1.innerHTML = `${rolls[0]}`
@@ -21,6 +30,7 @@ button.addEventListener('click', () => {
 
 });
 
+// RETURN HTML UNICODE 
 function roll(num){
     let returnNumber = Math.floor(Math.random() * num) + 1;
     let unicode = `&#${returnNumber + 9855};`
